49f86084c44cc34f71fd2109fd2deaa462723cdd
Lucas <lucasmaranhao150@gmail.com>
2026-01-11 18:41:13 -0300

    consoles tab redirect

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'homepage'
+app_name = 'store'
 
 urlpatterns = [
-    path('', views.home_view, name='home')
+    path('', views.home_view, name='home'),
+    path('consoles/', views.consoles_view, name='consoles')
 ]
--- store/views.py
+++ store/views.py
@@ -5,3 +5,7 @@ from django.http import HttpRequest
 
 def home_view(request):
     return render(request, 'store/home.html')
+
+
+def consoles_view(request):
+    return render(request, 'store/consoles.html')
